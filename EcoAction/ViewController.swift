7c33dcb66c73155bb14de4cfe5d6bada4b94c6cf
Tsai, Hsiao-Han <sunnyhome3224@gmail.com>
2019-05-25 19:31:05 +0800

    Initial Commit

---
+++ EcoAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoAction
+//
+//  Created by 蔡曉涵 on 25/5/19.
+//  Copyright © 2019 Orbital2019Team1952. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EcoActionTests/EcoActionTests.swift
@@ -0,0 +1,34 @@
+//
+//  EcoActionTests.swift
+//  EcoActionTests
+//
+//  Created by 蔡曉涵 on 25/5/19.
+//  Copyright © 2019 Orbital2019Team1952. All rights reserved.
+//
+
+import XCTest
+@testable import EcoAction
+
+class EcoActionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
